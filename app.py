5cce939666da764a85149b87e4b1e9e448261227
jetbomber <rwarwaruk@shaw.ca>
2019-10-28 15:36:22 -0600

    Update

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    name = request.args.get("name", "Run")
+    return f"<h1>Home, {escape(name)}!</h1>"
+
+
+@app.route("/about")
+def about():
+    name = request.args.get("name", "About Page")
+    return f"<h1>{escape(name)}!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
