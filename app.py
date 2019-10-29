7e4e7fca77667b9f6e4fc4b2b5d77b6adef2380a
jetbomber <rwarwaruk@shaw.ca>
2019-10-28 23:00:23 -0600

    Update

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Reagan Warwaruk",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "October 28, 2019",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "October 29, 2019",
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    name = request.args.get("name", "Run")
-    return f"<h1>Home, {escape(name)}!</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    name = request.args.get("name", "About Page")
-    return f"<h1>{escape(name)}!</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
